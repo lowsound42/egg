0cef4167ac20e96220a6bee5f803fb6bfe73cafc
Omar Khan <o.khandxb@gmail.com>
2020-02-29 14:41:48 -0500

    added useless component

--- src/components-new/Home.js
+++ src/components-new/Home.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import UselessInfo from './UselessInfo';
 import '../styles/home.scss';
 
 export default function Home() {
     return (
     <>
+        <Header />
         <div className='hero'>
-            <Header />
             <h1 className='hero__text'>The tomorrow of eggs . . .</h1>
             <h1 className='hero__text-sub'>is today</h1>
             <button className='hero__button'>Get in Touch</button>
         </div>
+        <UselessInfo/>
         <Footer/>
     </>
     );
---
+++ src/components-new/UselessInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/useless.scss';
+
+export default function UselessInfo() {
+    return (
+    <>
+        <div className='useless'>
+            <h1 className='useless__header'>Things don't just change. You have to change them.</h1>
+        </div>
+    </>
+    );
+  }
